188e468873158760f037f2ca8fe245658f344e51
Jimmy Alford <jimmy.alford@salesloft.com>
2022-12-17 21:17:30 -0500

    create game logic

---
+++ src/game.rs
@@ -0,0 +1,196 @@
+use crate::snake::Snake;
+use crate::point::Point;
+use crate::direction::Direction;
+use std::io::Stdout;
+use std::time::{Duration, Instant};
+use crossterm::terminal::size;
+use crate::command::Command;
+use rand::Rng;
+
+const MAX_INTERVAL: u16 = 700;
+const MIN_INTERVAL: u16 = 200;
+const MAX_SPEED: u16 = 20;
+
+#[derive(Debug)]
+pub struct Game {
+    stdout: Stdout,
+    original_terminal_size: (u16, u16),
+    width: u16,
+    height: u16,
+    food: Option<Point>,
+    snake: Snake,
+    speed: u16,
+    score: u16,
+}
+
+impl Game {
+    pub fn new(stdout: Stdout, width: u16, height: u16) -> Self {
+        let original_terminal_size: (u16, u16) = size().unwrap();
+        Self {
+            stdout,
+            original_terminal_size,
+            width,
+            height,
+            food: None,
+            snake: Snake::new(
+                Point::new(width / 2, height / 2),
+                3,
+                match rand::thread_rng().gen_range(0, 4) {
+                    0 => Direction::Up,
+                    1 => Direction::Right,
+                    2 => Direction::Down,
+                    _ => Direction::Left
+                },
+                ),
+                speed: 0,
+                score: 0,
+        }
+    }
+
+    pub fn run(&mut self) {
+        self.place_food();
+        self.prepare_ui();
+        self.render();
+
+        let mut done = false;
+        while !done {
+            let interval = self.calculate_interval();
+            let direction = self.snake.get_direction();
+            let now = Instant::now();
+
+            while now.elapsed() < interval {
+                if let Some(command) = self.get_command(interval - now.elapsed()) {
+                    match command {
+                        Command::Quit => {
+                            done = true;
+                            break;
+                        }
+                        Command::Turn(towards) => {
+                            if direction != towards && direction.opposite() != towards {
+                                self.snake.set_direction(towards);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if self.has_collided_with_wall() || self.has_bitten_itself() {
+                done = true;
+            } else {
+                self.snake.slither();
+
+                if let Some(food_point) = self.food {
+                    if self.snake.get_head_point() == food_point {
+                        self.snake.grow();
+                        self.place_food();
+                        self.score += 1;
+
+                        if self.score % ((self.width * self.height) / MAX_SPEED) == 0 {
+                            self.speed += 1;
+                        }
+                    }
+                }
+
+                self.render();
+            }
+        }
+
+        self.restore_ui();
+
+        println!("Game Over! Your score is {}", self.score);
+    }
+
+    fn place_food(&mut self) {
+        loop {
+            let random_x = rand::thread_rng().gen_range(0, self.width);
+            let random_y = rand::thread_rng().gen_range(0, self.height);
+            let point = Point::new(random_x, random_y);
+            if !self.snake.contains_point(&point) {
+                self.food = Some(point);
+                break;
+            }
+        }
+    }
+
+    fn prepare_ui(&mut self) {
+        enable_raw_mode().unwrap();
+        self.stdout
+            .execute(SetSize(self.width + 3, self.height + 3)).unwrap()
+            .execute(Clear(ClearType::All)).unwrap()
+            .execute(Hide).unwrap();
+    }
+
+    fn calculate_interval(&self) -> Duration {
+        let speed = MAX_SPEED - self.speed;
+        Duration::from_millis(
+            (MIN_INTERVAL + (((MAX_INTERVAL - MIN_INTERVAL) / MAX_SPEED) * speed)) as u64
+            )
+    }
+
+    fn get_command(&self, wait_for: Duration) -> Option<Command> {
+        let key_event = self.wait_for_key_event(wait_for)?;
+
+        match key_event.code {
+            KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Esc => Some(Command::Quit),
+            KeyCode::Char('c') | KeyCode::Char('C') =>
+                if key_event.modifiers == KeyModifiers::CONTROL {
+                    Some(Command::Quit)
+                } else {
+                    None
+                }
+            KeyCode::Up => Some(Command::Turn(Direction::Up)),
+            KeyCode::Right => Some(Command::Turn(Direction::Right)),
+            KeyCode::Down => Some(Command::Turn(Direction::Down)),
+            KeyCode::Left => Some(Command::Turn(Direction::Left)),
+            _ => None
+        }
+    }
+
+    fn wait_for_key_event(&self, wait_for: Duration) -> Option<KeyEvent> {
+        if poll(wait_for).ok()? {
+            let event = read().ok()?;
+            if let Event::Key(key_event) = event {
+                return Some(key_event);
+            }
+        }
+
+        None
+    }
+
+    let result = get_an_option();
+    let val = match result {
+        Some(value) => value,
+        None => return None,
+    }
+    let val = get_an_option()?;
+
+    fn has_collided_with_wall(&self) -> bool {
+        let head_point = self.snake.get_head_point();
+
+        match self.snake.get_direction() {
+            Direction::Up => head_point.y == 0,
+            Direction::Right => head_point.x == self.width - 1,
+            Direction::Down => head_point.y == self.height - 1,
+            Direction::Left => head_point.x == 0,
+        }
+    }
+
+    fn has_bitten_itself(&self) -> bool {
+        let next_head_point = self.snake.get_head_point().transform(self.snake.get_direction(), 1);
+        let mut next_body_points = self.snake.get_body_points().clone();
+        next_body_points.remove(next_body_points.len() - 1);
+        next_body_points.remove(0);
+
+        next_body_points.contains(&next_head_point)
+    }
+
+    fn restore_ui(&mut self) {
+        let (cols, rows) = self.original_terminal_size;
+        self.stdout
+            .execute(SetSize(cols, rows)).unwrap()
+            .execute(Clear(ClearType::All)).unwrap()
+            .execute(Show).unwrap()
+            .execute(ResetColor).unwrap();
+        disable_raw_mode().unwrap();
+    }
+}
